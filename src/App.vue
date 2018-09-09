b693d0f8e885fad8739bead6c0c48415c701e071
Alexander Buiko <reed665@hotmail.com>
2018-09-09 07:29:45 +0300

    setup base layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <header>
+      <h2>Header</h2>
+    </header>
+
+    <main>
+      <p>main content</p>
+    </main>
+
+    <footer>
+      Footer
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  overflow-y: scroll;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  font-family: sans-serif;
+  background: #eee;
+}
 #app {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#app header, #app footer {
+  background: white;
+  padding: 15px;
+}
+#app main {
+  padding: 15px;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
 }
 </style>
