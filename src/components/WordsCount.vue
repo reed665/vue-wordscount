35e9c3855e97a70bbc6fe1e292270d43166b2649
Alexander Buiko <reed665@hotmail.com>
2018-09-09 14:54:12 +0300

    use getWordsArray from wordscount package

--- src/components/WordsCount.vue
+++ src/components/WordsCount.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="container">
     <textarea class="box box-green" v-model="input"></textarea>
-    <p class="box box-blue">{{ output }}</p>
+
+    <div class="box box-blue">{{ output }}</div>
+
+    <div class="box box-pink" v-if="wordsArray && wordsArray.length">
+      <div v-for="(word, idx) of wordsArray" :key="idx">{{ word }}</div>
+    </div>
   </div>
 </template>
 
 <script>
-import wc from '@reed665/wordscount'
+import wordscount, { getWordsArray } from '@reed665/wordscount'
 
 export default {
   data() {
@@ -16,7 +21,10 @@ export default {
   },
   computed: {
     output() {
-      return wc(this.input)
+      return wordscount(this.input)
+    },
+    wordsArray() {
+      return getWordsArray(this.input)
     }
   }
 }
@@ -33,10 +41,14 @@ export default {
   padding: 15px;
   font-size: 1.5em;
   border-radius: 5px;
+  margin: 10px 0;
   background: lightgray;
 }
 .box-green {
   background: lightgreen;
+}
+.box-pink {
+  background: lightpink;
 }
 .box-blue {
   background: lightblue;
